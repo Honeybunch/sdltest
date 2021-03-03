5e267e43db8d6016c58258c7c0881ccad3a561a3
Honeybunch <amt3824@g.rit.edu>
2021-03-02 17:37:30 -0800

    quick fix

--- android/app/src/main/java/com/honeybunch/sdltest/SDLTestActivity.java
+++ android/app/src/main/java/com/honeybunch/sdltest/SDLTestActivity.java
@@ -26,7 +26,7 @@ public class SDLTestActivity extends SDLActivity {
         return new String[] {
                 "hidapi",
                 "SDL2",
-                "imgui_testbed"
+                "sdltest"
         };
     }
 }
