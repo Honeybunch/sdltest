367ed861f3ae70bccea094d3f2ab803f2657f11b
Honeybunch <amt3824@g.rit.edu>
2021-10-21 14:00:00 -0700

    Actually enabling nxlink stdio

--- switch/src/entry.c
+++ switch/src/entry.c
@@ -11,6 +11,8 @@ int32_t main(int32_t argc, char *argv[]) {
   romfsInit();
   // TODO: Only if a debug build...
   socketInitializeDefault();
+  nxlink_socket = nxlinkStdio();
+
   int32_t ret = SDL_main(argc, argv);
 
   if (nxlink_socket != -1) {
