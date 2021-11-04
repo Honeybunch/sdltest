48d7d3d714d33a676dc51249841b29e0f8f32695
Honeybunch <amt3824@g.rit.edu>
2021-11-03 17:01:17 -0700

    Updating install deps script

--- install_deps.bat
+++ install_deps.bat
@@ -2,31 +2,10 @@
 
 cd ./vcpkg
 
-REM For now, get DXC from Vulkan SDK, which is still kind of required
-REM Install Windows Tools
-REM call vcpkg install tool-dxc:x64-windows
-
-REM Install Windows Deps
-call vcpkg install cgltf:x64-windows
-call vcpkg install imgui:x64-windows
-call vcpkg install ktx[vulkan,tools]:x64-windows
-call vcpkg install mimalloc:x64-windows
-call vcpkg install sdl2[vulkan]:x64-windows
-call vcpkg install sdl2-image:x64-windows
-call vcpkg install volk:x64-windows
-call vcpkg install vulkan-memory-allocator:x64-windows
-call vcpkg install tracy:x64-windows
+call vcpkg install tool-dxc:x64-windows cgltf:x64-windows imgui:x64-windows ktx[vulkan,tools]:x64-windows mimalloc:x64-windows sdl2[vulkan]:x64-windows sdl2-image:x64-windows volk:x64-windows vulkan-memory-allocator:x64-windows tracy:x64-windows
 
 REM Install Windows Static Deps
-call vcpkg install cgltf:x64-windows-static
-call vcpkg install imgui:x64-windows-static
-call vcpkg install ktx[vulkan,tools]:x64-windows-static
-call vcpkg install mimalloc:x64-windows-static
-call vcpkg install sdl2[vulkan]:x64-windows-static
-call vcpkg install sdl2-image:x64-windows-static
-call vcpkg install volk:x64-windows-static
-call vcpkg install vulkan-memory-allocator:x64-windows-static
-call vcpkg install tracy:x64-windows-static
+call vcpkg install  tool-dxc:x64-windows-static cgltf:x64-windows-static imgui:x64-windows-static ktx[vulkan,tools]:x64-windows-static mimalloc:x64-windows-static sdl2[vulkan]:x64-windows-static sdl2-image:x64-windows-static volk:x64-windows-static vulkan-memory-allocator:x64-windows-static tracy:x64-windows-static
 
 REM Return to starting directory
 cd %~dp0
