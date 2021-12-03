c8f711f3a0abc6d6811d0cb336ce493e5e0607a5
Honeybunch <amt3824@g.rit.edu>
2021-12-02 16:48:10 -0800

    Updating deps

--- install_switch_deps.bat
+++ install_switch_deps.bat
@@ -3,7 +3,7 @@
 cd ./vcpkg
 
 REM Install Switch Deps
-call vcpkg install cgltf:arm64-switch imgui:arm64-switch ktx[vulkan]:arm64-switch mimalloc:arm64-switch sdl2[vulkan]:arm64-switch sdl2-image:arm64-switch volk:arm64-switch vulkan-memory-allocator:arm64-switch tracy:arm64-switch
+call vcpkg install meshoptimizer:arm64-switch cgltf:arm64-switch imgui:arm64-switch ktx[vulkan]:arm64-switch mimalloc:arm64-switch sdl2[vulkan]:arm64-switch sdl2-image:arm64-switch volk:arm64-switch vulkan-memory-allocator:arm64-switch tracy:arm64-switch
 
 REM Return to starting directory
 cd %~dp0
